493f3457f212907776612709f53c9efc6a08afd1
sdeffontaines <sarah.deffontaines@capgemini.com>
2024-08-08 12:04:26 +0200

    add typing

--- mapie/anomaly_control.py
+++ mapie/anomaly_control.py
@@ -17,12 +17,12 @@ class AnomalyControl:
 
     fit_attributes = ["ano_th"]
 
-    def __init__(self, ano_detector, alpha, delta):
+    def __init__(self, ano_detector, alpha: float, delta: float) -> None:
         self.ano_detector = ano_detector
         self.alpha = self.check_alpha_delta(alpha)
         self.delta = self.check_alpha_delta(delta)
 
-    def check_alpha_delta(self, alpha: float):
+    def check_alpha_delta(self, alpha: float) -> float:
         """
         Check alpha.
 
@@ -55,7 +55,7 @@ class AnomalyControl:
             )
         return alpha
 
-    def _check_ano_detector_fitted(self, X_calib) -> None:
+    def _check_ano_detector_fitted(self, X_calib: np.array) -> None:
         """
         Check if the anomaly detector is fitted to the training dataset
 
@@ -81,7 +81,7 @@ class AnomalyControl:
         self.check_alpha_delta(self.alpha)
         self.check_alpha_delta(self.delta)
 
-    def fit_ano_detector(self, X_train):
+    def fit_ano_detector(self, X_train: np.array) -> "AnomalyControl":
         """
         Fit the anomaly detector chosen to the training dataset
 
@@ -93,7 +93,7 @@ class AnomalyControl:
         self.ano_detector = self.ano_detector.fit(X_train)
         return self
 
-    def fit_calibrator(self, X_calib, y_calib):
+    def fit_calibrator(self, X_calib: np.array, y_calib: np.array) -> "AnomalyControl":
         # check if ood_detector is fitted
         self._check_ano_detector_fitted(X_calib)
         # check alpha and delta
@@ -130,12 +130,14 @@ class AnomalyControl:
 
         return self
 
-    def fit(self, X_train, X_calib, y_calib):
+    def fit(
+        self, X_train: np.array, X_calib: np.array, y_calib: np.array
+    ) -> "AnomalyControl":
         self.fit_ano_detector(X_train)
         self.fit_calibrator(X_calib, y_calib)
         return self
 
-    def predict(self, X):
+    def predict(self, X: np.array) -> np.array(bool):
         check_is_fitted(self, self.fit_attributes)
 
         ano_scores = self.ano_detector.predict(X)
